803cf6c833ec74524c62f56bb36b3d44f38b36f1
Avi Kenny <avi.kenny@gmail.com>
2024-07-24 16:58:28 -0400

    Updated to DCC cluster

--- R/config.R
+++ R/config.R
@@ -1,5 +1,5 @@
 # Change library path
-.libPaths(c("/home/akenny/R_lib", .libPaths()))
+.libPaths(c("/home/akenny/R_lib", "/hpc/home/ak811/R_lib", .libPaths()))
 
 # Set packages
 cfg$pkgs <- c(
@@ -15,18 +15,18 @@ if (Sys.getenv("HOME")=="/home/akenny") {
     dir = paste0("/home/akenny/", Sys.getenv("proj"),
                  "/Code__", Sys.getenv("proj"))
   )
-} else if (Sys.getenv("HOME")=="/home/users/avikenny") {
-  # Bayes
+} else if (Sys.getenv("HOME")=="/hpc/home/ak811") {
+  # DCC
   cluster_config <- list(
-    js = "ge",
-    dir = paste0("/home/users/avikenny/Desktop/", Sys.getenv("proj"),
+    js = "slurm",
+    dir = paste0("/hpc/home/ak811/", Sys.getenv("proj"),
                  "/Code__", Sys.getenv("proj"))
   )
 } else {
   cluster_config <- list(js="", dir="")
 }
 
-# Load packages
+# Load packages (if running locally or not running sims)
 for (pkg in c(cfg$pkgs,cfg$pkgs_nocluster)) {
   suppressMessages({ do.call("library", list(pkg)) })
 }
